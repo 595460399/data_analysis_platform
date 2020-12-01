7b741dc2c47768b5e86d3e3b974276bb1fb8c7ad
leeyo <595460399@qq.com>
2020-12-02 02:15:57 +0800

    测试2

--- data_analysis_platform/chpa_data/views.py
+++ data_analysis_platform/chpa_data/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
+
     context = {'data': 'data'}
+
     return render(request, 'chpa_data/index.html', context)
--- data_analysis_platform/data_analysis_platform/urls.py
+++ data_analysis_platform/data_analysis_platform/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chpa/', include(('chpa_data.urls', 'chpa_data'), namespace='chpa')),
